89af8129d3e9b85124518296c9e3bc771b791ada
Federico G. Ramos <federicoramos@me.com>
2024-01-22 16:04:54 +0100

    Initial Commit

---
+++ CustomPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomPicker
+//
+//  Created by Federico G. Ramos on 22.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomPicker/CustomPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPickerApp.swift
+//  CustomPicker
+//
+//  Created by Federico G. Ramos on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CustomPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
